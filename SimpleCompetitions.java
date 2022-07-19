e5bc03fa78b89368d25895ab0461bf6e543aec35
Amelia-Wu <62229810+Amelia-Wu@users.noreply.github.com>
2022-07-19 16:39:55 +1000

    Update SimpleCompetitions.java

--- SimpleCompetitions.java
+++ SimpleCompetitions.java
@@ -1,9 +1,3 @@
-/*
- * Student name: Xinyi Wu
- * Student ID: 1306826
- * LMS username: wuxw13
- */
-
 /**
  * This class is used to start the app as the first step and process all the commands.
  *
